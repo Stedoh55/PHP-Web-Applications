8e4650e9c4d530cedf530b6324262de638fb45df
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-05-07 08:14:16 +0300

    The PHP files for the week 5 Assignment

---
+++ Week 5 Assignment/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Steven Simoni PHP</title>
+</head>
+<body>
+	<h1>Steven Simoni PHP</h1>
+	<p>
+	<?php
+	print 'The SHA256 hash of "Steven Simoni" is ' . hash('sha256', 'Steven Simoni');
+	?>
+	</p>
+	
+	<pre>
+	<?php
+	echo "\n";
+	echo "ASCII ART\n";
+	echo "\n";
+	echo "          **\n";
+	echo "        ******\n";
+	echo "     ***     ***\n";
+	echo "    **         **\n";
+	echo "    **         *\n";
+	echo "     *****\n";
+	echo "       ******\n";
+	echo "            ***\n";
+	echo "    *        ***\n";
+	echo "   **          **\n";
+	echo "    ****      ***\n";
+	echo "       ******\n";
+	echo "          **\n";
+
+	?>
+	</pre>
+		
+
+</body>
+</html>
