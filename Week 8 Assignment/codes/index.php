2c501b13210bb37604738fda767724a6cf2047ef
Steven Simoni <157221633+Stedoh55@users.noreply.github.com>
2024-05-15 11:27:35 +0300

    The Workout on the Rock Paper Scissor game

---
+++ Week 8 Assignment/codes/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Steven Simoni d63e5b61</title>
+<?php require_once "bootstrap.php"; ?>
+</head>
+<body>
+<div class="container">
+<h1>Welcome to Rock Paper Scissors</h1>
+<p>
+<a href="login.php">Please Log In</a>
+</p>
+<p>
+Attempt to go to 
+<a href="game.php">game.php</a> without logging in - it should fail with an error message.
+
+</div>
+</body>
+
